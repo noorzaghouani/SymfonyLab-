02ac66fc9f8fdb76332e21793c739a34ae129f3d
noorzaghouani <noor.zaghouani@gmail.com>
2025-12-07 12:23:26 +0100

    lab6

---
+++ lab6/var/cache/dev/twig/1b/1bee80ee5c12a025a78a59acd786f8ec.php
@@ -0,0 +1,117 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\CoreExtension;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+use Twig\TemplateWrapper;
+
+/* voiture/updateVoiture.html.twig */
+class __TwigTemplate_7bd5157134a9dbf550914309bf06260c extends Template
+{
+    private Source $source;
+    /**
+     * @var array<string, Template>
+     */
+    private array $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = []): iterable
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "voiture/updateVoiture.html.twig"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "voiture/updateVoiture.html.twig"));
+
+        // line 1
+        yield "<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <meta name=\"viewport\" content=\"width=<1>, initial-scale=1.0\">
+    <title>Update voiture</title>
+</head>
+<body>
+    <h1>MAJ d'une voiture </h1>
+    <form action=\"\"method=\"Post\">
+        ";
+        // line 11
+        yield $this->env->getRuntime('Symfony\Component\Form\FormRenderer')->searchAndRenderBlock((isset($context["editFormVoiture"]) || array_key_exists("editFormVoiture", $context) ? $context["editFormVoiture"] : (function () { throw new RuntimeError('Variable "editFormVoiture" does not exist.', 11, $this->source); })()), 'widget');
+        yield "
+        <input type=\"submit\" value=\"MAJ Voiture\">
+    </form>
+</body>
+</html>";
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        yield from [];
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName(): string
+    {
+        return "voiture/updateVoiture.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo(): array
+    {
+        return array (  60 => 11,  48 => 1,);
+    }
+
+    public function getSourceContext(): Source
+    {
+        return new Source("<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <meta name=\"viewport\" content=\"width=<1>, initial-scale=1.0\">
+    <title>Update voiture</title>
+</head>
+<body>
+    <h1>MAJ d'une voiture </h1>
+    <form action=\"\"method=\"Post\">
+        {{ form_widget(editFormVoiture) }}
+        <input type=\"submit\" value=\"MAJ Voiture\">
+    </form>
+</body>
+</html>", "voiture/updateVoiture.html.twig", "C:\\xampp\\htdocs\\tp_symfony\\lab6\\templates\\voiture\\updateVoiture.html.twig");
+    }
+}
